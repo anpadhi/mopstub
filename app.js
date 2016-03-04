da40e3613327ebb818b4a408dd6491cff4052450
Anshuman Padhi <anshupadhi@gmail.com>
2016-03-04 16:42:17 +0800

    models updated

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ var customers = require('./routes/customers');
 var invoices = require('./routes/invoices');
 var offers = require('./routes/offers');
 
+
+//provide a sensible default for local development
+var db_name = 'mopstub';
+var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
 // Mongoose Connect
-mongoose.connect('mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/mopstub');
+mongoose.connect(mongodb_connection_string);
 var db = mongoose.connection;
 
 app.use(express.static(__dirname+'/client'))	;
@@ -22,8 +31,8 @@ app.use('/api/customers', customers);
 app.use('/api/invoices', invoices);
 app.use('/api/offers', offers);
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+//app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+//app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 //var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 //var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
@@ -31,7 +40,9 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 //app.listen(port, ip);
 //console.log('Started on port 3000...' + ip + ':'+port);
 var http = require('http');
-http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
