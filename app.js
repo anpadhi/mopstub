34c4143d3b5ecc7f10cb5e6bbd0b68ee86a2e05b
Anshuman Padhi <anshupadhi@gmail.com>
2016-03-04 12:30:08 +0800

    Added files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var customers = require('./routes/customers');
+var invoices = require('./routes/invoices');
+var offers = require('./routes/offers');
+
+// Mongoose Connect
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
+
+app.use(express.static(__dirname+'/client'))	;
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.send('Please use /api/customers or /api/invoices');
+});
+
+app.use('/api/customers', customers);
+app.use('/api/invoices', invoices);
+app.use('/api/offers', offers);
+
+app.listen(3000);
+console.log('Started on port 3000...');
